72ff459d6bbe7e04d1abad1272e0b513a0ecc253
Kamal Boorghani <kamal@boorghani.com>
2025-05-02 20:40:36 -0400

    Implement Phase 1 Step 1: Home Row Mods for programmers layout

---
+++ config/include/keypos_helios.h
@@ -0,0 +1,111 @@
+#pragma once
+
+// Key positions for Kinesis Advantage 2 (Helios)
+
+// Left hand
+#define L_ESC    0
+#define L_F1     1
+#define L_F2     2
+#define L_F3     3
+#define L_F4     4
+#define L_F5     5
+#define L_F6     6
+#define L_F7     7
+#define L_F8     8
+
+#define L_EQL    18
+#define L_1      19
+#define L_2      20
+#define L_3      21
+#define L_4      22
+#define L_5      23
+
+#define L_TAB    24
+#define L_Q      25
+#define L_W      26
+#define L_E      27
+#define L_R      28
+#define L_T      29
+
+#define L_CAPS   30
+#define L_A      31
+#define L_S      32
+#define L_D      33
+#define L_F      34
+#define L_G      35
+
+#define L_LSHIFT 36
+#define L_Z      37
+#define L_X      38
+#define L_C      39
+#define L_V      40
+#define L_B      41
+
+#define L_GRV    42
+#define L_BSLH   43
+#define L_LEFT   44
+#define L_RIGHT  45
+
+#define L_LCTRL  46
+#define L_LGUI   47
+#define L_HOME   48
+#define L_BSPC   49
+#define L_DEL    50
+#define L_END    51
+
+// Right hand
+#define R_F9     9
+#define R_F10    10
+#define R_F11    11
+#define R_F12    12
+#define R_PSCR   13
+#define R_SLCK   14
+#define R_PAUSE  15
+#define R_LAYER1 16
+#define R_LAYER2 17
+
+#define R_6      52
+#define R_7      53
+#define R_8      54
+#define R_9      55
+#define R_0      56
+#define R_MINUS  57
+
+#define R_Y      58
+#define R_U      59
+#define R_I      60
+#define R_O      61
+#define R_P      62
+#define R_BSLH2  63
+
+#define R_H      64
+#define R_J      65
+#define R_K      66
+#define R_L      67
+#define R_SEMI   68
+#define R_SQT    69
+
+#define R_N      70
+#define R_M      71
+#define R_COMMA  72
+#define R_DOT    73
+#define R_SLASH  74
+#define R_RSHIFT 75
+
+#define R_UP     76
+#define R_DOWN   77
+#define R_LBKT   78
+#define R_RBKT   79
+
+#define R_RALT   80
+#define R_RCTRL  81
+#define R_PGUP   82
+#define R_PGDN   83
+#define R_ENTER  84
+#define R_SPACE  85
+
+// Key position groups
+#define KEYS_L L_A L_S L_D L_F L_G L_Q L_W L_E L_R L_T L_Z L_X L_C L_V L_B
+#define KEYS_R R_H R_J R_K R_L R_SEMI R_Y R_U R_I R_O R_P R_N R_M R_COMMA R_DOT R_SLASH
+
+#define THUMBS L_LCTRL L_LGUI L_HOME L_BSPC L_DEL L_END R_RALT R_RCTRL R_PGUP R_PGDN R_ENTER R_SPACE 
